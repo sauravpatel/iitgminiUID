7e1717013c5a0f7a79757384b334c75301ac8e2d
saurav <saurav@saurav-HCL-Desktop.(none)>
2013-04-08 02:40:21 +0530

    working on search

--- controllers/search.py
+++ controllers/search.py
@@ -1,4 +1,25 @@
 # controller for search
 
 def index():
-	return dict( message = T( "Welcome to search" ) )
+    field = getFieldList()
+    userOptions = ['student', 'faculty', 'staff', 'others' ]
+    form = SQLFORM.factory(
+            Field( 'userOptions',widget=SQLFORM.widgets.checkboxes.widget, requires=[IS_IN_SET(userOptions, multiple=True), IS_NOT_EMPTY()] ), keepvalues=True)
+    if form.process().accepted:
+        redirect( URL( makeQuery, args=[form.vars['userOptions'][0] ] ) )
+    #return dict( message = T( "Welcome to search" ) )
+    return dict( message = field[0] , form=form)
+
+def getFieldList():
+    fields=['name','dob'];
+    #response.flash( "Selecting fields." )
+    return fields
+
+def makeQuery():
+    request.vars._formname = 'queryform'
+    fields=['student.name', 'student.rollno', 'faculty.room', 'student.webmail']
+    #form = SQLFORM.factory(
+            #Field('where', 'string', notnull=True,requires=IS_IN_SET(fields)) )
+    #if form.accepts(request.vars, formname='myform'):
+        #return 'success'
+    return dict(fields=fields, i=0)
--- static/js/search.js
+++ static/js/search.js
@@ -7,18 +7,20 @@ function getSubField()
 		others : 0
 	};	if( document.criteriaForm.student.checked == true ){
 		selected.student = true;
-		alert( "You have checked student." );
+		//alert( "You have checked student." );
 	}
 	if( document.criteriaForm.faculty.checked == true ){
 		selected.faculty = true;
-		alert( "You have checked faculty." );
+		//alert( "You have checked faculty." );
 	}
 	if( document.criteriaForm.staff.checked == true ){
 		selected.staff = true;
-		alert( "You have checked staff." );
+		//alert( "You have checked staff." );
 	}
 	if( document.criteriaForm.others.checked == true ){
 		selected.others = true;
-		alert( "You have checked others." );
+		//alert( "You have checked others." );
 	}
+    self.location = "{{=URL('getFieldList')}}";
+    //field = [ "name", "dob" ];
 }
